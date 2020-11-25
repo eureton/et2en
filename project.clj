8b439fe4debfcfa743916fa4bb65b359af4d1a94
eureton <agis@mailbox.org>
2020-11-25 19:03:39 +0200

    draft project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject et2en "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Estonian-English translator for the command line."
+  :url "https://github.com/eureton/et2en.git"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
