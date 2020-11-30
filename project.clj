5df515ddcea51cb358efa89b4918e9612301557b
eureton <agis@mailbox.org>
2020-11-30 12:21:42 +0200

    core.async project dep

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [enlive "1.1.1"]
                  [org.jsoup/jsoup "1.7.3"]
-                 [clj-http "3.10.3"]]
+                 [clj-http "3.10.3"]
+                 [org.clojure/core.async "1.3.610"]]
   :main ^:skip-aot et2en.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
